6594107fc5876f356cde1d71fe8bbed910f191f8
eka0789 <67857639+eka0789@users.noreply.github.com>
2020-08-22 07:05:20 +0700

    Latihan-PHP

---
+++ page18/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // Definisikan function hello
+    
+    
+    // Panggil function hello
+    
+    
+    echo '<br>';
+    // Definisikan function printRectangleArea
+    
+    
+    // Panggil function printRectangleArea dengan (5, 10) sebagai argument
+    
+    
+  ?> <?php
+
+    // Definisikan function hello
+    function hello() {
+      echo 'Hello, world!';
+    }
+    
+    // Panggil function hello
+    hello();
+    
+    echo '<br>';
+    // Definisikan function printRectangleArea
+    function printRectangleArea($height, $width) {
+      echo $height * $width;
+    }
+    
+    // Panggil function printRectangleArea dengan (5, 10) sebagai argument
+    printRectangleArea(5, 10);
+    
+  ?>
+
+
+</body>
+</html>
